db70656640514a84e59708db7fb00f12e6573f9f
Hunain Parekh <hunain.parekh@xloopdigital.com>
2023-05-05 10:02:16 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Timeline from "react-calendar-timeline";
+// make sure you include the timeline stylesheet or the timeline will not be styled
+import "react-calendar-timeline/lib/Timeline.css";
+import moment from "moment";
+import { useState, useEffect } from "react";
+
+const groups = [
+  { id: 1, title: "group 1" },
+  { id: 2, title: "group 2" },
+];
 
 function App() {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    for (let i = 0; i < 1000; i++) {
+      let startDate = moment("2022-01-01");
+      let endDate = moment("2023-12-31");
+      let start = moment(startDate + Math.random() * (endDate - startDate));
+      let end = moment(start + Math.random() * (endDate - start));
+      setItems((prev) => [
+        ...prev,
+        {
+          id: i,
+          group: 1,
+          title: "item " + i,
+          start_time: start,
+          end_time: end,
+        },
+      ]);
+    }
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Rendered by react!
+      <Timeline
+        groups={groups}
+        items={items}
+        defaultTimeStart={moment().add(-12, "hour")}
+        defaultTimeEnd={moment().add(12, "hour")}
+      />
     </div>
   );
 }
